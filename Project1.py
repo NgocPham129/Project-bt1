61b5dea5bb0200a99f03f22489b12a532a26a49c
NgocPham129 <154904519+NgocPham129@users.noreply.github.com>
2023-12-26 22:09:55 +0700

    Project1.py

---
+++ Project1.py
@@ -0,0 +1,16 @@
+Bài 1 
+print('Hello world')
+
+Bài 2
+a='Hello word'
+print(a)
+
+Bài 3
+b='Hello'
+c='World'
+d=b+' '+c
+
+Bài 4
+def print_hello_world(var):
+    print(var)
+print_hello_world(d)
